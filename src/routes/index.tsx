e71e61e0078fd89988aa751c5df8ea3949923607
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-17 16:01:49 -0300

    adicionando react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {}
+import { Navigate, Route, Routes } from "react-router-dom";
+
+function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<p>Oi</p>}/>
+            <Route path="*" element={<Navigate to='/'/>}/>
+        </Routes>
+    );
+}
+
+export default AppRoutes;

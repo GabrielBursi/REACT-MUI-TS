f838f992cb2c755fa0d63752740456633237c765
Gabriel Bursi <gabriellbursii@gmail.com>
2023-01-24 16:45:04 -0300

    criando hook para impedir consultas desnecessarias

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,27 +1,29 @@
 import { useEffect, useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
+import {useDebounce} from "../../shared/hooks";
 import { LayoutBase } from "../../shared/layouts";
 import { pessoasServices } from "../../shared/services/api/pessoas/ApiServices";
 
 function ListagemDePessoas() {
 
     const [searchParams, setSearchParams] = useSearchParams()
+    const { debounce } = useDebounce()
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || ''
     }, [searchParams])
 
     useEffect(() => {
-        pessoasServices.getAll()
-            .then(data => {
-                if(data instanceof Error){
-                    alert(data.message)
-                }else{
-                    console.log(data);
-                }
-            })
-    }, []);
+        debounce(async () => {
+            const data = await pessoasServices.getAll()
+            if(data instanceof Error){
+                alert(data.message)
+            }else{
+                console.log(data);
+            }
+        })
+    }, [busca]);
     
     return (
         <LayoutBase 
--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,1 +1,1 @@
-export {}
+export {default as useDebounce} from './useDebounce'
